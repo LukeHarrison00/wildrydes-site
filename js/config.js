f3d2c956ded8313e0097142a341a7ad33cff113f
LukeHarrison00 <160607408+LukeHarrison00@users.noreply.github.com>
2025-10-06 14:46:10 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_VgzlhbpAP', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1jd1bqscgh4jvmg74oigmastft', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
